8b664b195bafa64f827ea8ead5961eba3b37ba4f
Devibhargavi <134496990+Devibhargavi@users.noreply.github.com>
2023-05-24 16:32:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.haeder('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 & bluebeery oatmeal')
 streamlit.text('kale, spinach & rocket smoothie')
 streamlit.text('Hard boiled free range Egg')
